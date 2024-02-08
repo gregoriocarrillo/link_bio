c3a6d8b87b1dcefbc787ec7516fefcc60eb6c92e
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2024-02-08 16:55:29 -0400

    actualizacion header

--- link_bio/views/header/header.py
+++ link_bio/views/header/header.py
@@ -5,5 +5,5 @@ def header() -> rx.Component:
         rx.avatar(name="Gregorio Carrillo", size="xl"),
         rx.text("@roswel47"),
         rx.text("GREGORIO CARRILLO"),
-        rx.text("Soy entusiasta desarrollador web, programador y formándome en ciberseguridad. Adicional soy fotógrafo profesional y corredor de seguros, feliz esposo y padre de 2 niñas. Aquí encontrarán enlaces de interés")
+        rx.text("Soy un entusiasta desarrollador web y formándome en ciberseguridad. Adicional soy fotógrafo profesional y corredor de seguros, feliz esposo y padre de 2 niñas. Aquí encontrarán enlaces de interés")
     )
